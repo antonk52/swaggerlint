fc31d0a804bfcff77977b42a11b40faaf4ab9ed1
antonk52 <antonk52@yandex-team.ru>
2019-12-17 01:14:13 +0300

    feat(types): mimicing swagger spec

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,8 @@
 /*
+ * All types and their names should mimic the swagger spec.
+ *
+ * https://swagger.io/specification
+ *
  * TODO
  * https://github.com/OAI/OpenAPI-Specification/blob/master/versions/2.0.md
  */
@@ -11,15 +15,33 @@ export type Config = {
     };
 };
 
-export type Info = {
-    description: string;
-    version: string;
+/**
+ * https://swagger.io/specification/#contactObject
+ */
+export type ContactObject = {
+    name?: string;
+    url?: string;
+    email?: string;
+};
+
+/**
+ * https://swagger.io/specification/#licenseObject
+ */
+export type LicenceObject = {
+    name: string;
+    url?: string;
+};
+
+/**
+ * https://swagger.io/specification/#infoObject
+ */
+export type InfoObject = {
     title: string;
-    contact: {
-        name: string;
-        url: string;
-        email: string;
-    };
+    description?: string;
+    termsOfService?: string;
+    contact?: ContactObject;
+    licence?: string;
+    version: string;
 };
 export type Tag = {
     name: string;
@@ -35,27 +57,32 @@ export type XMLObject = {
 };
 
 export type PropBase<A = Object> = {
-  title?: string,
-  description?: string;
+    title?: string;
+    description?: string;
 } & A;
 
+type StrFormat = 'byte' | 'binary' | 'date' | 'date-time' | 'password';
 export type PropString = PropBase<{
     type: 'string';
-    format?: 'byte' | 'binary' | 'date' | 'date-time' | 'password';
+    format?: StrFormat;
     default?: string;
     enum?: string[];
 }>;
+
+type IntFormat = 'int32' | 'int64';
 export type PropInteger = PropBase<{
     type: 'integer';
-    format?: 'int32' | 'int64';
+    format?: IntFormat;
     description?: string;
     default?: number;
     minimum?: number;
     maximum?: number;
 }>;
+
+type NumberFormat = 'float' | 'double';
 export type PropNumber = PropBase<{
     type: 'number';
-    format?: 'float' | 'double';
+    format?: NumberFormat;
     default?: number;
     minimum?: number;
     maximum?: number;
@@ -96,7 +123,9 @@ export type Property =
     | PropString
     | PropInteger
     | PropNumber
-    | PropBoolean;
+    | PropBoolean
+    | PropArray;
+
 export type DefinitionObject = {
     title: string;
     description?: string;
@@ -109,14 +138,210 @@ export type DefinitionObject = {
     externalDocs?: any;
     example?: any;
 };
-export type Swagger = {
+
+type RestSchema =
+    | {
+          $ref: string;
+      }
+    | {
+          type: 'array';
+          items:
+              | {
+                    $ref: string;
+                }
+              | {
+                    type:
+                        | 'object'
+                        | 'boolean'
+                        | 'string'
+                        | 'number'
+                        | 'integer';
+                };
+      };
+export type PathParameter = {
+    name: string;
+    in: 'query' | 'path' | 'body';
+    description: string;
+    required?: boolean;
+    // query only
+    allowEmptyValue?: boolean;
+} & Property;
+// TODO
+export type ExternalDocumentationObject = {};
+
+/**
+ * https://swagger.io/specification/#parameterObject
+ */
+export type ParameterObject = {
+    name: string;
+    // TODO break into sep types by `in`
+    in: 'query' | 'header' | 'path' | 'cookie';
+    description: string;
+    required?: boolean;
+    deprecated?: boolean;
+    allowEmptyValue?: boolean;
+};
+
+/**
+ * https://swagger.io/specification/#referenceObject
+ */
+export type ReferenceObject = {
+    $ref: string;
+};
+
+// TODO
+export type RequestBodyObject = {};
+// TODO
+export type SecurityRequirementObject = {};
+
+/**
+ * https://swagger.io/specification/#serverVariableObject
+ */
+export type ServerVariableObject = {
+    enum?: string[];
+    default: string;
+    description?: string;
+};
+
+/**
+ * https://swagger.io/specification/#serverObject
+ */
+export type ServerObject = {
+    url: string;
+    description: string;
+    variables: {
+        [n: string]: ServerVariableObject;
+    };
+};
+// TODO
+export type CallbackObject = {};
+/**
+ * https://swagger.io/specification/#operationObject
+ */
+export type OperationObject = {
+    tags?: string[];
+    summary?: string;
+    description?: string;
+    externalDocs?: ExternalDocumentationObject;
+    operationId: string;
+    // parameters: PathParameter[];
+    parameters: (ParameterObject | ReferenceObject)[];
+    requestBody?: RequestBodyObject | ReferenceObject;
+    responses: {
+        [responseCode: string]: {
+            description: string;
+            schema?: RestSchema;
+        };
+    };
+    callbacks?: {
+        [s: string]: CallbackObject | ReferenceObject;
+    };
+    deprecated?: boolean;
+    consumes: string[];
+    security: SecurityRequirementObject[];
+    servers: ServerObject[];
+};
+
+/**
+ * https://swagger.io/specification/#pathItemObject
+ */
+export type PathItemObject = {
+    $ref?: string;
+    summary?: string;
+    description?: string;
+    get?: OperationObject;
+    post?: OperationObject;
+    put?: OperationObject;
+    delete?: OperationObject;
+    options?: OperationObject;
+    trace?: OperationObject;
+    servers?: ServerObject[];
+    parameters?: (ParameterObject | ReferenceObject)[];
+};
+
+/**
+ * https://swagger.io/specification/#pathsObject
+ */
+export type PathsObject = {
+    [m: string]: PathItemObject;
+};
+
+// TODO
+export type SchemaObject = {};
+// TODO
+export type ResponseObject = {};
+// TODO
+export type ExampleObject = {};
+// TODO
+export type HeaderObject = {};
+// TODO
+export type SecuritySchemeObject = {};
+// TODO
+export type LinkObject = {};
+
+/**
+ * https://swagger.io/specification/#componentsObject
+ */
+export type ComponentsObject = {
+    schemas: {
+        [m: string]: SchemaObject | ReferenceObject;
+    };
+    responses: {
+        [m: string]: ResponseObject | ReferenceObject;
+    };
+    parameters: {
+        [m: string]: ParameterObject | ReferenceObject;
+    };
+    examples: {
+        [m: string]: ExampleObject | ReferenceObject;
+    };
+    requestBodies: {
+        [m: string]: RequestBodyObject | ReferenceObject;
+    };
+    headers: {
+        [m: string]: HeaderObject | ReferenceObject;
+    };
+    securitySchemes: {
+        [m: string]: SecuritySchemeObject | ReferenceObject;
+    };
+    links: {
+        [m: string]: LinkObject | ReferenceObject;
+    };
+    callbacks: {
+        [m: string]: CallbackObject | ReferenceObject;
+    };
+};
+
+/**
+ * https://swagger.io/specification/#tagObject
+ */
+export type TagObject = {
+    name: string;
+    description?: string;
+    externalDocs?: ExternalDocumentationObject;
+};
+
+/**
+ * https://swagger.io/specification/#openapi-object
+ */
+export type OpenAPIObject = {
+    /**
+     * Semantic version numver
+     */
+    openapi: string; // semantic version
+    info: InfoObject;
+    servers?: ServerObject[];
+    paths: PathsObject;
+    components?: ComponentsObject;
+    security?: SecurityRequirementObject[];
+    tags: TagObject[];
+    externalDocs?: ExternalDocumentationObject;
+
+    // below are questionable
     swagger: string;
-    info: Info;
     host: string;
     basePath: string;
-    tags: Tag[];
-    // TODO
-    paths: any;
+    // tags: Tag[];
     definitions: {
         [k: string]: Property;
     };
@@ -128,6 +353,6 @@ export type LintError = {
 };
 
 export type Rule = {
-    name: string,
-    check: (a: Swagger, b: string[], c?: Config) => LintError[],
+    name: string;
+    check: (a: OpenAPIObject, b: string[], c?: Config) => LintError[];
 };
