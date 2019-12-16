7e10afaf2d2eb907c771ac1295564b7c5c24ce08
antonk52 <antonk52@yandex-team.ru>
2019-12-17 01:15:32 +0300

    feat(rules): path-param-required-field

--- src/rules/index.ts
+++ src/rules/index.ts
@@ -1,11 +1,13 @@
 import objPropCasing from './object-prop-casing';
 import propertiesForObjectType from './properties-for-object-type';
 import latinDefinitionsOnly from './latin-definitions-only';
+import pathParamRequiredField from './path-param-required-field';
 
 const rules = {
     [objPropCasing.name]: objPropCasing.check,
     [propertiesForObjectType.name]: propertiesForObjectType.check,
     [latinDefinitionsOnly.name]: latinDefinitionsOnly.check,
+    [pathParamRequiredField.name]: pathParamRequiredField.check,
 };
 
 export default rules;
---
+++ src/rules/path-param-required-field/index.ts
@@ -0,0 +1,75 @@
+import {
+    Rule,
+    LintError,
+    PathItemObject,
+    PathsObject,
+    ReferenceObject,
+} from '../../types';
+
+const name = 'path-param-required-field';
+
+const methods: ['get', 'post', 'put', 'delete', 'options', 'trace'] = [
+    'get',
+    'post',
+    'put',
+    'delete',
+    'options',
+    'trace',
+];
+
+function isRefObj(obj: any): obj is ReferenceObject {
+    return (
+        typeof obj === 'object' &&
+        !Array.isArray(obj) &&
+        obj !== null &&
+        '$ref' in obj &&
+        typeof obj.$ref === 'string'
+    );
+}
+
+const rule: Rule = {
+    name,
+    check: swagger => {
+        const errors: LintError[] = [];
+
+        Object.keys(swagger.paths).forEach((pathName: keyof PathsObject) => {
+            const path: PathItemObject = swagger.paths[pathName];
+            if (path.parameters) {
+                path.parameters.forEach(param => {
+                    if (isRefObj(param)) {
+                        // TODO handle refs
+                        return;
+                    }
+                    if (typeof param.required !== 'boolean') {
+                        errors.push({
+                            name,
+                            msg: `required is not set for path "${pathName}".parameters`,
+                        });
+                    }
+                });
+            }
+            methods.forEach(method => {
+                const operation = path[method];
+                if (operation === undefined) return;
+                console.log(operation);
+                operation.parameters.forEach(param => {
+                    if (isRefObj(param)) {
+                        // TODO handle refs
+                        return;
+                    }
+                    if (typeof param.required !== 'boolean') {
+                        errors.push({
+                            name,
+                            msg: `required is not set for path "${pathName}" in "${method}" in "${param.name}"`,
+                        });
+                    }
+
+                });
+            });
+        });
+
+        return errors;
+    },
+};
+
+export default rule;
