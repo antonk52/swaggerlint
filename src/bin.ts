b9b66b0d44ab8136dcc1ab11bb2bf8ebf13d06d1
antonk52 <antonk52@yandex-team.ru>
2020-01-08 23:17:59 +0300

    feat(cli): help tag

--- src/bin.ts
+++ src/bin.ts
@@ -8,13 +8,35 @@ import {green} from 'kleur';
 const pkg = require('../package.json');
 
 function program() {
-    const {version, v, ...options} = minimist<CliOptions>(
+    const {version, v, help, h, ...options} = minimist<CliOptions>(
         process.argv.slice(2),
     );
 
     if (version || v) {
         console.log(pkg.version);
 
+        process.exit(0);
+    }
+
+    if (help || h) {
+        console.log(
+            `
+
+swaggerlint version ${pkg.version}
+<${pkg.repository.url.replace('git+', '').replace('.git', '')}>
+swaggerlint is a program to lint swagger (OpenAPI v2.0) API specification.
+
+Usage:
+    swaggerlint ./path/to/swagger/file.json
+    swaggerlint http://url.to/swagger/file.yml
+
+Options:
+ -v, --version              print version number
+ -h, --help                 show this help
+ --config                   add user config (swaggerlint.config.js)
+
+            `.trim(),
+        );
         process.exit(0);
     }
 
