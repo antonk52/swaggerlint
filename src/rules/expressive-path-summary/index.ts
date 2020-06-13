076f24658173a7a0b71a703eaca4cab1444b029c
antonk52 <halloy52@gmail.com>
2020-06-13 14:25:29 +0300

    feat(rules): expressive-path-summary refine error location with tests

--- src/rules/expressive-path-summary/index.ts
+++ src/rules/expressive-path-summary/index.ts
@@ -5,12 +5,13 @@ const name = 'expressive-path-summary';
 const rule: SwaggerlintRule = {
     name,
     visitor: {
-        OperationObject: ({node, report}): void => {
+        OperationObject: ({node, report, location}): void => {
             const {summary} = node;
-            if (summary) {
+            if (typeof summary === 'string') {
                 if (summary.split(' ').length < 2) {
                     report(
                         `Every path summary should contain at least 2 words. This has "${summary}"`,
+                        [...location, 'summary'],
                     );
                 }
             } else {
