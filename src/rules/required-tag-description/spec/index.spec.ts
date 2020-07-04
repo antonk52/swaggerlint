ac86ec292f8abbac7e4dcb49826e80910a34ff8c
antonk52 <halloy52@gmail.com>
2020-07-05 00:43:44 +0300

    openapiVisitor and tests for required-tag-description rule

--- src/rules/required-tag-description/spec/index.spec.ts
+++ src/rules/required-tag-description/spec/index.spec.ts
@@ -1,10 +1,20 @@
 import rule from '../';
-import {Swagger, SwaggerlintConfig} from '../../../types';
+import {Swagger, SwaggerlintConfig, OpenAPI} from '../../../types';
 import {swaggerlint} from '../../../';
 import _merge from 'lodash.merge';
 
 const swaggerSample: Swagger.SwaggerObject = {
     swagger: '2.0',
+    info: {
+        title: 'stub',
+        version: '1.0',
+    },
+    paths: {},
+    tags: [],
+};
+
+const openapiSample: OpenAPI.OpenAPIObject = {
+    openapi: '3.0.3',
     info: {
         title: 'stub',
         version: '1.0',
@@ -20,34 +30,59 @@ describe(`rule "${rule.name}"`, () => {
         },
     };
 
-    it('should NOT error for an empty swagger sample', () => {
-        const result = swaggerlint(swaggerSample, config);
+    const mod = {
+        tags: [
+            {
+                name: 'no-description',
+            },
+            {
+                name: 'with-description',
+                description: 'some description about the tag',
+            },
+        ],
+    };
 
-        expect(result).toEqual([]);
+    describe('swagger', () => {
+        it('should NOT error for an empty swagger sample', () => {
+            const result = swaggerlint(swaggerSample, config);
+
+            expect(result).toEqual([]);
+        });
+
+        it('should error for a tag missing description', () => {
+            const modConfig = _merge(mod, swaggerSample);
+            const result = swaggerlint(modConfig, config);
+            const expected = [
+                {
+                    msg: 'Tag "no-description" is missing description.',
+                    name: 'required-tag-description',
+                    location: ['tags', '0'],
+                },
+            ];
+
+            expect(result).toEqual(expected);
+        });
     });
 
-    it('should error for a tag missing description', () => {
-        const mod = {
-            tags: [
-                {
-                    name: 'no-description',
-                },
-                {
-                    name: 'with-description',
-                    description: 'some description about the tag',
-                },
-            ],
-        };
-        const modConfig = _merge(mod, swaggerSample);
-        const result = swaggerlint(modConfig, config);
-        const expected = [
-            {
-                msg: 'Tag "no-description" is missing description.',
-                name: 'required-tag-description',
-                location: ['tags', '0'],
-            },
-        ];
+    describe('openapi', () => {
+        it('should NOT error for an empty swagger sample', () => {
+            const result = swaggerlint(openapiSample, config);
 
-        expect(result).toEqual(expected);
+            expect(result).toEqual([]);
+        });
+
+        it('should error for a tag missing description', () => {
+            const modConfig = _merge(mod, openapiSample);
+            const result = swaggerlint(modConfig, config);
+            const expected = [
+                {
+                    msg: 'Tag "no-description" is missing description.',
+                    name: 'required-tag-description',
+                    location: ['tags', '0'],
+                },
+            ];
+
+            expect(result).toEqual(expected);
+        });
     });
 });
