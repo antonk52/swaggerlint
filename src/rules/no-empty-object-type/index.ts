5eabbcd21525d8e68e9d51b5f7b7c413b00770c0
antonk52 <halloy52@gmail.com>
2020-07-26 15:33:26 +0300

    fix allOf in swagger walker and tests for no-empty-object-type rule

--- src/rules/no-empty-object-type/index.ts
+++ src/rules/no-empty-object-type/index.ts
@@ -1,5 +1,4 @@
 import {SwaggerlintRule} from '../../types';
-import {isRef} from '../../utils/swagger';
 
 const name = 'no-empty-object-type';
 
@@ -7,43 +6,43 @@ const rule: SwaggerlintRule = {
     name,
     swaggerVisitor: {
         SchemaObject: ({node, report}): void => {
-            if (isRef(node)) return;
             if (node.type !== 'object') return;
 
-            if ('properties' in node && node.properties) {
-                return;
-            } else if ('allOf' in node && node.allOf) {
-                return;
-            } else if (
-                'additionalProperties' in node &&
-                node.additionalProperties
-            ) {
-                return;
-            } else {
-                report(
-                    `has "object" type but is missing "properties" | "additionalProperties" | "allOf"`,
-                );
-            }
+            const hasProperties = 'properties' in node && !!node.properties;
+            const hasAllOf = 'allOf' in node && !!node.allOf;
+            const hasAdditionalProperties =
+                'additionalProperties' in node && !!node.additionalProperties;
+
+            if (hasProperties || hasAllOf || hasAdditionalProperties) return;
+
+            report(
+                `has "object" type but is missing "properties" | "additionalProperties" | "allOf"`,
+            );
         },
     },
     openapiVisitor: {
         SchemaObject: ({node, report}): void => {
             if (node.type !== 'object') return;
 
-            if ('properties' in node && node.properties) {
+            const hasProperties = 'properties' in node && !!node.properties;
+            const hasAllOf = 'allOf' in node && !!node.allOf;
+            const hasAnyOf = 'anyOf' in node && !!node.anyOf;
+            const hasOneOf = 'oneOf' in node && !!node.oneOf;
+            const hasAdditionalProperties =
+                'additionalProperties' in node && !!node.additionalProperties;
+
+            if (
+                hasProperties ||
+                hasAllOf ||
+                hasAnyOf ||
+                hasOneOf ||
+                hasAdditionalProperties
+            )
                 return;
-            } else if ('allOf' in node && node.allOf) {
-                return;
-            } else if (
-                'additionalProperties' in node &&
-                node.additionalProperties
-            ) {
-                return;
-            } else {
-                report(
-                    `has "object" type but is missing "properties" | "additionalProperties" | "allOf"`,
-                );
-            }
+
+            report(
+                `has "object" type but is missing "properties" | "additionalProperties" | "allOf" | "anyOf" | "oneOf"`,
+            );
         },
     },
 };
